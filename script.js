c17fee0fe8af7c0bf23a3481ef9abb0751f411dd
lucian diaconu <luciandiaconu123@gmail.com>
2024-12-23 01:32:37 +0100

    folder structure refactor, added contact form

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.querySelector('.menu-toggle').addEventListener("click", function() {
   document.querySelector(".nav").classList.toggle("mobile-nav");
   this.classList.toggle("is-active");
 });
+document.querySelector('.nav').addEventListener("click", function() {
+  document.querySelector(".nav").classList.toggle("mobile-nav");
+  document.querySelector('.menu-toggle').classList.toggle("is-active");
+});
